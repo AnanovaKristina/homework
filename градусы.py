b7f94d74b0df4406c18f25df4f94519439a9b18b
AnanovaKristina <92989218+AnanovaKristina@users.noreply.github.com>
2021-10-23 00:54:28 +0700

    Add files via upload

---
+++ градусы.py
@@ -0,0 +1,4 @@
+print("a=?")
+import math
+a=float(input("введите значение в цельсиях"))
+print(a*9/5+32)
---
+++ длина стороны треугольника.py
@@ -0,0 +1,14 @@
+import math
+print ("введите значение длин сторон")
+print("a=?")
+a=int(input())
+print("b=?")
+b=int(input())
+print("y=?")
+y=int(input())
+print(math.sqrt(a*a+b*b-2*a*b*math.cos(math.radians(y))))
+
+
+
+
+
